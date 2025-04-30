c98936f99d263fd2df4f1a52e2787350ac55baab
Elcand <tjandraraharja99@gmail.com>
2025-04-30 15:34:27 +0700

    PENGKONDISIAN : SWITCH pada JAVASCRIPT

--- switch.js
+++ switch.js
@@ -1,16 +1,18 @@
-var angka = parseInt(prompt("masukan angka:"));
+var item = prompt(
+  "masukan nama makanan / minuman : \n (cth: nasi, daging, susu, hamburger, softdrink)"
+);
 
-switch (angka) {
-  case 1:
-    alert("anda memasukan angka 1");
+switch (item) {
+  case 'nasi':
+  case 'daging':
+  case 'susu':
+    alert("makanan / minuman SEHAT!");
     break;
-  case 2:
-    alert("anda memasukan angka 2");
-    break;
-  case 3:
-    alert("anda memasukan angka 3");
+  case 'hamburger':
+  case 'softdrink':
+    alert("minuman TIDAK SEHAT!");
     break;
   default:
-    alert("anda memasukan angka salah");
+    alert("makanan / minuman yg anda masukan salah");
     break;
 }
