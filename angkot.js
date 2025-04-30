b025c0c5f4d3cbaba8e0c207071aba1f3222e30c
Elcand <tjandraraharja99@gmail.com>
2025-04-30 14:43:06 +0700

    PENGKONDISIAN : Latihan Pengkondisian pada JAVASCRIPT

--- angkot.js
+++ angkot.js
@@ -3,9 +3,9 @@ var angkotBeroperasi = 6;
 var noAngkot = 1;
 
 for (noAngkot >= angkotBeroperasi; noAngkot <= jmlAngkot; noAngkot++) {
-  if (noAngkot < 7) {
+  if (noAngkot < 7 && noAngkot != 5) {
     console.log("Angkot No. " + noAngkot + " beroperasi dengan baik");
-  } else if (noAngkot == 8) {
+  } else if (noAngkot == 8 || noAngkot == 10 || noAngkot == 5) {
     console.log("Angkot No. " + noAngkot + " sedang lembur");
   } else {
     console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi");
