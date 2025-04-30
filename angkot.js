84d0ecd0b61022b65cf42515ccb409c6eb9c7b48
Elcand <tjandraraharja99@gmail.com>
2025-04-30 10:20:15 +0700

    PENGKONDISIAN : IF ELSE pada JAVASCRIPT

--- angkot.js
+++ angkot.js
@@ -1,10 +1,10 @@
 var jmlAngkot = 10;
-var angkotBeroperasi = 7;
+var angkotBeroperasi = 6;
 var noAngkot = 1;
-while (noAngkot <= angkotBeroperasi) {
-  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik");
-  noAngkot++;
-}
-for (noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
-  console.log("Angkot No. " + noAngkot + " sedang tidak  beroperasi");
+for (noAngkot >= angkotBeroperasi; noAngkot <= jmlAngkot; noAngkot++) {
+  if (noAngkot <= 7) {
+    console.log("Angkot No. " + noAngkot + " sedang beroperasi");
+  } else {
+    console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi");
+  }
 }
