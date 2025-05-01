2d345aabad606705c91f06a0269d716433508450
Elcand <tjandraraharja99@gmail.com>
2025-05-01 14:41:03 +0700

    belajar menjalankan function

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-var nilaiAwal = 1;
-while (nilaiAwal <= 5) {
-  console.log("Halo dunia" + nilaiAwal);
-  nilaiAwal++;
+function jumlahVolumeDuaKubus(a, b) {
+  var VA;
+  var VB;
+  var total;
+
+  VA = a * a * a;
+  VB = b * b * b;
+
+  total = VA + VB;
+
+  return total;
 }
+alert(jumlahVolumeDuaKubus(8, 3));
+alert(jumlahVolumeDuaKubus(10, 15));
