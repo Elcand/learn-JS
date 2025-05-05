821529e61ffa0acad7fffd8b1b54144e56d9a902
Elcand <tjandraraharja99@gmail.com>
2025-05-05 10:56:52 +0700

    belajar function expression

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-function jumlahVolumeDuaKubus(a, b) {
-  var VA;
-  var VB;
-  var total;
+var popUp = function (name) {
+  alert("halo " + name);
+};
 
-  VA = a * a * a;
-  VB = b * b * b;
-
-  total = VA + VB;
-
-  return total;
-}
-alert(jumlahVolumeDuaKubus(8, 3));
-alert(jumlahVolumeDuaKubus(10, 15));
+popUp('ikel'); //harus mendefinisikaan variable terlebih dahulu
