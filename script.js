7360cfaa9aac1bf6c67940880f20710adae80d46
Elcand <tjandraraharja99@gmail.com>
2025-05-05 13:41:20 +0700

    belajar push & pop

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var arr = ["stroberry", "mangga", "apel", "kates", "gedang"];
+
+// push -> menambahakan array
+// arr.push("kelapa", "melon");
+
+// pop -> menghilangkan array terakhir
+// arr.pop();
+// arr.pop();
+
 console.log(arr.join(" . "));
