8f682237a3759e0b10d88b396d647ed50c619bcb
Elcand <tjandraraharja99@gmail.com>
2025-05-06 15:25:33 +0700

    learn function declaration

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-function buatObjectMahasiswa(nama, nrp, email, jurusan) {
-  var mhs = {};
-  mhs.nama = nama;
-  mhs.nrp = nrp;
-  mhs.email = email;
-  mhs.jurusan = jurusan;
-  return mhs;
-}
+// function buatObjectMahasiswa(nama, nrp, email, jurusan) {
+//   var mhs = {};
+//   mhs.nama = nama;
+//   mhs.nrp = nrp;
+//   mhs.email = email;
+//   mhs.jurusan = jurusan;
+//   return mhs;
+// }
 
-var mhs1 = buatObjectMahasiswa("mikhael", "220308", "mikhael@gmail.com", "IT");
-var mhs2 = buatObjectMahasiswa(
-  "candra",
-  "032208",
-  "cand@gmail.com",
-  "Keuangan"
-);
+// var mhs1 = buatObjectMahasiswa("mikhael", "220308", "mikhael@gmail.com", "IT");
+// var mhs2 = buatObjectMahasiswa(
+//   "candra",
+//   "032208",
+//   "cand@gmail.com",
+//   "Keuangan"
+// );
 
-// constractor
-function Mahasiswa(nama, nrp, email, jurusan) {
-  this.nama = nama;
-  this.nrp = nrp;
-  this.email = email;
-  this.jurusan = jurusan;
-}
+// // constractor
+// function Mahasiswa(nama, nrp, email, jurusan) {
+//   this.nama = nama;
+//   this.nrp = nrp;
+//   this.email = email;
+//   this.jurusan = jurusan;
+// }
 
-var mhs3 = new Mahasiswa("ikel", "56789876", "ikel123@gmail.com", "Animasi");
+// var mhs3 = new Mahasiswa("ikel", "56789876", "ikel123@gmail.com", "Animasi");
+
+// membuat object
+
+// function declaration
+// function halo() {
+//   console.log("halo");
+// }
